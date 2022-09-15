7842119b16f62e777e6fcdd842ea2d2364f09112
olga <oklimovich37@gmail.com>
2022-09-15 13:47:36 +0200

    newtest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import randomColor from 'randomcolor';
+import {v4 as uuidv4} from 'uuid';
+import Draggable from 'react-draggable';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const[item, setItem]=useState('')
+
+  const[items, setItems]=useState(
+    JSON.parse(localStorage.getItem('items'))||[]
+  )
+
+  const newItem=()=>{
+    if(item.trim()!==''){
+      const newItem={
+        id: uuidv4(),
+        item: item,
+        color: randomColor({
+          luminosity:'bright'
+        }),
+        defaultPos:{
+          x: 500,
+          y: -500
+        }
+      }
+      setItems((items)=>[...items, newItem])
+      setItem('')
+    }else{
+      alert('Enter task...')
+      setItem('')
+    }
+
+  }
+
+  const deleteItem=(id)=>{
+    setItems(items.filter((item)=>item.id!==id))
+  }
+
+  const updatePos=(data, index)=>{
+    let newArr=[...items]
+    newArr[index].defaultPos={x: data.x, y: data.y}
+    setItems(newArr)
+  }
+
+  useEffect(()=>{
+    localStorage.setItem('items',JSON.stringify(items))
+  },[items])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        <input 
+          className='input'
+          type='text'
+          placeholder='Enter task...'
+          onChange={(e)=>setItem(e.target.value)}
+          ></input>
+        <button className='button' onClick={newItem}>ENTER</button>
+      </div>
+      {
+        items.map((item, index)=>{
+          return(
+            <Draggable
+             key={index}
+             defaultPosition={item.defaultPos}
+             onStop={(_, data)=>{updatePos(data, index)}}
+             
+            >
+
+              <div className='todo_item' style={{backgroundColor: item.color}}>
+                {`${item.item}`}
+                <button 
+                 className='delete'
+                 onClick={()=>deleteItem(item.id)}>Х</button>
+                 
+              </div>
+
+             </Draggable>
+          )
+        })
+      }
     </div>
   );
 }
